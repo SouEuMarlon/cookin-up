bf33accdf0c5a8d63de4c2edc7ff06877a808ac9
Marlon <marlon_menezes@outlook.com>
2024-02-15 18:55:00 -0300

    Feat: Projeto finalizado offline

---
+++ src/components/SuaLista.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+import type { PropType } from 'vue';
+import Tag from './Tag.vue';
+export default {
+
+  props: {
+    ingredientes: {
+      type: Array as PropType<string[]>,
+      required: true
+    }
+  },  
+  components: { Tag }
+}
+</script>
+
+<template>
+  <section>
+    <span class="subtitulo-lg sua-lista-texto">
+      Sua lista:
+    </span>
+
+    <ul class="ingredientes-sua-lista" v-if="ingredientes.length">
+      <li v-for="(ingrediente, index) in ingredientes" :key="index">
+        <Tag :texto="ingrediente" ativa/>
+      </li>
+    </ul>
+
+    <p class="paragrafo lista-vazia" v-else>
+      <img src="../assets/images/icones/lista-vazia.svg" alt="Icone de pesquisa">
+      Sua lista esta vazia. Selecione ingredientes para iniciar.
+    </p>
+  </section>
+</template>
+
+<style scoped>
+
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+
+</style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export default {
+    props: {
+      texto: {
+        type: String,
+        required: true
+      },
+      ativa: Boolean
+    }
+  }
+</script>
+
+<template>
+  <span :class="['tag', { ativa }]">
+    {{ texto }}
+  </span>
+</template>
+
+<style scoped>
+.tag {
+  background: var(--cinza-claro);
+  border-radius: 0.5rem;
+  color: var(--cinza);
+  display: inline-block;
+  font-weight: 400;
+  min-width: 4.25rem;
+  padding: 0.5rem;
+  text-align: center;
+  transition: 0.2s;
+}
+
+.tag.ativa {
+  color: var(--creme, #FFFAF3);
+  background: var(--coral, #F0633C);
+  font-weight: 700;
+}
+
+
+</style>
